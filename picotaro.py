8891c94b75180541c9b555e751b69c65a7c79f49
strangekid024 <80091332+strangekid024@users.noreply.github.com>
2021-03-07 17:54:46 +0900

    first_cimmit

---
+++ picotaro.py
@@ -0,0 +1,9 @@
+def mk_pen(fruits, pen):
+    print ("ppap")
+    print ("i have a" + pen)
+    print ("i have a" + fruits)
+    fruits_pen = fruits + pen
+    print ("mmmm"+ fruits_pen )
+
+
+mk_pen("apple","pen")
